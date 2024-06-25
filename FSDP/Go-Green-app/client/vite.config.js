a593b4e3bd51cfe80a97a2d5c9e5782917f9177b
Toaster <135201735+ToasterSSD@users.noreply.github.com>
2024-06-26 04:41:09 +0800

    cahnges

--- FSDP/Go-Green-app/client/vite.config.js
+++ FSDP/Go-Green-app/client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,3 +9,17 @@ export default defineConfig({
     port: 3000
   }
 })
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy:{
+//       '/api': {
+//         target: "http://localhost:PORT",
+//         changeOrigin: true
+//       }
+//     }
+//   },
+//   plugins: [react()],
+//   port: 3000
+// })
