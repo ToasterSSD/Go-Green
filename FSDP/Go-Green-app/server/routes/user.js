22fbb7eef4beaff1c35309ded9da7fa0972829aa
ToasterSSD <asherhol7@gmail.com>
2024-06-26 11:07:10 +0800

    chnages

--- FSDP/Go-Green-app/server/routes/user.js
+++ FSDP/Go-Green-app/server/routes/user.js
@@ -1,104 +1,162 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const { User } = require('../models');
+const bcrypt = require("bcrypt");
+const { User } = require("../models");
 const yup = require("yup");
-const { sign } = require('jsonwebtoken');
-const { validateToken } = require('../middlewares/auth');
-require('dotenv').config();
+const { sign } = require("jsonwebtoken");
+const { validateToken } = require("../middlewares/auth");
+require("dotenv").config();
+
+const adminEmail = "admin@example.com";
+const adminPassword = bcrypt.hashSync("adminpassword", 8);
 
 router.post("/register", async (req, res) => {
-    let data = req.body;
-    // Validate request body
-    let validationSchema = yup.object({
-        name: yup.string().trim().min(3).max(50).required()
-            .matches(/^[a-zA-Z '-,.]+$/,
-                "name only allow letters, spaces and characters: ' - , ."),
-        email: yup.string().trim().lowercase().email().max(50).required(),
-        password: yup.string().trim().min(8).max(50).required()
-            .matches(/^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/,
-                "password at least 1 letter and 1 number")
+  let data = req.body;
+  // Validate request body
+  let validationSchema = yup.object({
+    name: yup
+      .string()
+      .trim()
+      .min(3)
+      .max(50)
+      .required()
+      .matches(
+        /^[a-zA-Z '-,.]+$/,
+        "name only allow letters, spaces and characters: ' - , ."
+      ),
+    email: yup.string().trim().lowercase().email().max(50).required(),
+    password: yup
+      .string()
+      .trim()
+      .min(8)
+      .max(50)
+      .required()
+      .matches(
+        /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/,
+        "password at least 1 letter and 1 number"
+      ),
+  });
+  try {
+    data = await validationSchema.validate(data, { abortEarly: false });
+
+    // Check email
+    let user = await User.findOne({
+      where: { email: data.email },
     });
-    try {
-        data = await validationSchema.validate(data,
-            { abortEarly: false });
-
-        // Check email
-        let user = await User.findOne({
-            where: { email: data.email }
-        });
-        if (user) {
-            res.status(400).json({ message: "Email already exists." });
-            return;
-        }
-
-        // Hash passowrd
-        data.password = await bcrypt.hash(data.password, 10);
-        // Create user
-        let result = await User.create(data);
-        res.json({
-            message: `Email ${result.email} was registered successfully.`
-        });
+    if (user) {
+      res.status(400).json({ message: "Email already exists." });
+      return;
     }
-    catch (err) {
-        res.status(400).json({ errors: err.errors });
-    }
+
+    // Hash passowrd
+    data.password = await bcrypt.hash(data.password, 10);
+    // Create user
+    let result = await User.create(data);
+    res.json({
+      message: `Email ${result.email} was registered successfully.`,
+    });
+  } catch (err) {
+    res.status(400).json({ errors: err.errors });
+  }
 });
 
 router.post("/login", async (req, res) => {
-    let data = req.body;
-    // Validate request body
-    let validationSchema = yup.object({
-        email: yup.string().trim().lowercase().email().max(50).required(),
-        password: yup.string().trim().min(8).max(50).required()
+  let data = req.body;
+  // Validate request body
+  let validationSchema = yup.object({
+    email: yup.string().trim().lowercase().email().max(50).required(),
+    password: yup.string().trim().min(8).max(50).required(),
+  });
+  try {
+    data = await validationSchema.validate(data, { abortEarly: false });
+
+    // Check email and password
+    let errorMsg = "Email or password is not correct.";
+    let user = await User.findOne({
+      where: { email: data.email },
     });
-    try {
-        data = await validationSchema.validate(data,
-            { abortEarly: false });
-
-        // Check email and password
-        let errorMsg = "Email or password is not correct.";
-        let user = await User.findOne({
-            where: { email: data.email }
-        });
-        if (!user) {
-            res.status(400).json({ message: errorMsg });
-            return;
-        }
-        let match = await bcrypt.compare(data.password, user.password);
-        if (!match) {
-            res.status(400).json({ message: errorMsg });
-            return;
-        }
-
-        // Return user info
-        let userInfo = {
-            id: user.id,
-            email: user.email,
-            name: user.name
-        };
-        let accessToken = sign(userInfo, process.env.APP_SECRET,
-            { expiresIn: process.env.TOKEN_EXPIRES_IN });
-        res.json({
-            accessToken: accessToken,
-            user: userInfo
-        });
+    if (!user) {
+      res.status(400).json({ message: errorMsg });
+      return;
     }
-    catch (err) {
-        res.status(400).json({ errors: err.errors });
-        return;
+    let match = await bcrypt.compare(data.password, user.password);
+    if (!match) {
+      res.status(400).json({ message: errorMsg });
+      return;
     }
+
+    // Return user info
+    let userInfo = {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    };
+    let accessToken = sign(userInfo, process.env.APP_SECRET, {
+      expiresIn: process.env.TOKEN_EXPIRES_IN,
+    });
+    res.json({
+      accessToken: accessToken,
+      user: userInfo,
+    });
+  } catch (err) {
+    res.status(400).json({ errors: err.errors });
+    return;
+  }
 });
 
 router.get("/auth", validateToken, (req, res) => {
-    let userInfo = {
-        id: req.user.id,
-        email: req.user.email,
-        name: req.user.name
-    };
-    res.json({
-        user: userInfo
+  let userInfo = {
+    id: req.user.id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.json({
+    user: userInfo,
+  });
+  
+router.post("/adminlogin", async (req, res) => {
+let data = req.body;
+// Validate request body
+let validationSchema = yup.object({
+    adminemail: yup.string().trim().lowercase().email().max(50).required(),
+    adminpassword: yup.string().trim().min(8).max(50).required(),
+});
+try {
+    data = await validationSchema.validate(data, { abortEarly: false });
+
+    // Check email and password
+    let errorMsg = "Email or password is not correct.";
+    let user = await User.findOne({
+    where: { adminemail: data.adminemail },
     });
+    if (!user) {
+    res.status(400).json({ message: errorMsg });
+    return;
+    }
+    let match = await bcrypt.compare(data.adminpassword, user.adminpassword);
+    if (!match) {
+    res.status(400).json({ message: errorMsg });
+    return;
+    }
+    // Return user info
+    let userInfo = {
+    id: user.id,
+    adminemail: user.adminemail,
+    name: user.name,
+    };
+    let accessToken = sign(userInfo, process.env.APP_SECRET, {
+    expiresIn: process.env.TOKEN_EXPIRES_IN,
+    });
+    res.json({
+    accessToken: accessToken,
+    user: userInfo,
+    });
+} catch (err) {
+    res.status(400).json({ errors: err.errors });
+    return;
+}
+});
+
 });
 
 module.exports = router;
