580f76a3967e9dc8b128c14123dcd62556f99143
Jason Hong <126115162+JasonHong06@users.noreply.github.com>
2024-07-10 11:12:20 +0800

    Revert ".history shit"
    This reverts commit b64a75e243862e3831ad0ee1f4ae8aa02bb80716.

---
+++ .history/github-jason/learning-app/server/routes/chatarea_20240626004950.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const { User, ChatArea } = require("../models");
+const { Op } = require("sequelize");
+const yup = require("yup");
+const { validateToken } = require("../middlewares/auth");
+
+router.post("/", validateToken, async (req, res) => {
+  let data = req.body;
+  data.userId = req.user.id;
+  // Validate request body
+  let validationSchema = yup.object({
+    title: yup.string().trim().min(3).max(200).required(),
+    content: yup.string().trim().min(3).max(1000).required(),
+  });
+  try {
+    data = await validationSchema.validate(data, { abortEarly: false });
+    let result = await ChatArea.create(data);
+    res.json(result);
+  } catch (err) {
+    res.status(400).json({ errors: err.errors });
+  }
+});
+
+// show all announcements
+router.get("/", async (req, res) => {
+  let condition = {};
+  let search = req.query.search;
+  if (search) {
+    condition[Op.or] = [
+      { title: { [Op.like]: `%${search}%` } },
+      { content: { [Op.like]: `%${search}%` } },
+    ];
+  }
+  // You can add condition for other columns here
+  // e.g. condition.columnName = value;
+
+  let list = await ChatArea.findAll({
+    where: condition,
+    order: [["createdAt", "DESC"]],
+    include: { model: User, as: "user", attributes: ["name"] },
+  });
+  res.json(list);
+});
+
+
+module.exports = router;
