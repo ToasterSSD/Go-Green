a14743443bc4f06492bda4495230bc965f08fc56
ToasterSSD <asherhol7@gmail.com>
2024-07-29 19:38:15 +0800

    added lou bin items

--- Go-Green/FSDP/server/index.js
+++ Go-Green/FSDP/server/index.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const cors = require('cors');
 require('dotenv').config();
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const cors = require('cors');
+const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,6 +27,8 @@ const feedbackRoutes = require('./routes/feedback');
 const fileRoute = require('./routes/file');
 const announcementRoute = require('./routes/announcement');
 const chatareaRoute = require('./routes/chatarea');
+const articleRoute = require('./routes/article');
+const learningRoute = require('./routes/learning');
 
 app.use("/tutorial", tutorialRoute);
 app.use("/user", userRoute);
@@ -31,15 +36,39 @@ app.use("/feedback", feedbackRoutes);
 app.use("/file", fileRoute);
 app.use("/announcement", announcementRoute);
 app.use("/chatarea", chatareaRoute);
+app.use("/article", articleRoute);
+app.use("/learning", learningRoute);
+
+// Route for public learning topics
+app.get('/public-learning-topics', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Adjust the path as needed
+});
+
+// Route for public articles
+app.get('/public-articles', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Adjust the path as needed
+});
+
+// Set up body-parser middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// Create a MySQL connection
+// const gogreendb = mysql.createConnection({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: 'mysql',
+//     database: 'gogreendb'
+// });
 
 const db = require('./models');
 db.sequelize.sync({ alter: true })
     .then(() => {
         let port = process.env.APP_PORT;
+
         app.listen(port, () => {
             console.log(`⚡ Server running on http://localhost:${port}`);
         });
     })
     .catch((err) => {
         console.log(err);
-    });
+    });
---
+++ Go-Green/FSDP/server/models/Article.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+    const Article = sequelize.define(
+        "Article", 
+        {
+        title: {
+            type: DataTypes.STRING(30),
+            allowNull: false
+        },
+        category: {
+            type: DataTypes.STRING(30),
+            allowNull: false
+        },
+        author: {
+            type: DataTypes.STRING(30),
+            allowNull: false
+        },
+        imageFile: {
+            type: DataTypes.STRING(255)
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+        
+    }, {
+        tableName: 'articles'
+    });
+
+    return Article;
+}
