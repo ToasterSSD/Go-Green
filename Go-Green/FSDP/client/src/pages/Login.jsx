92df3e81fa419b483be2b10694de241d1287bb2c
ToasterSSD <asherhol7@gmail.com>
2024-07-30 15:01:28 +0800

    Update Login.jsx

--- Go-Green/FSDP/client/src/pages/Login.jsx
+++ Go-Green/FSDP/client/src/pages/Login.jsx
@@ -34,7 +34,7 @@ function Login() {
                 .then((res) => {
                     localStorage.setItem("accessToken", res.data.accessToken);
                     setUser(res.data.user);
-                    navigate("/admin");
+                    navigate("/");
                 })
                 .catch(function (err) {
                     toast.error(`${err.response.data.message}`);
